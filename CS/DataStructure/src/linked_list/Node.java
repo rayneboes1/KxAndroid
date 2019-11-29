e37e54ff92d7ac6bdd38dcab6aceb6523914e5f1
wenhaiz <wenhaiz.me@qq.com>
2019-11-29 21:41:11 +0800

    增加链表结点定义和单元测试

---
+++ CS/DataStructure/src/linked_list/Node.java
@@ -0,0 +1,19 @@
+package linked_list;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public static String link2String(Node head) {
+        if (head == null) {
+            return "";
+        }
+        String result = head.data + "";
+        result += "," + link2String(head.next);
+        return result;
+    }
+}
---
+++ CS/DataStructure/test/linked_list/NodeTest.java
@@ -0,0 +1,34 @@
+package linked_list;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NodeTest {
+    private Node list;
+
+    @Before
+    public void initList(){
+        list = new Node(1);
+        list.next = new Node(2);
+        list.next.next = new Node(3);
+        list.next.next.next=new Node(4);
+    }
+
+
+    @Test
+    public void testNode2String(){
+        assertEquals("1,2,3,4,",Node.link2String(list));
+    }
+
+    @Test
+    public void testNode2StringWithNull(){
+        assertEquals("",Node.link2String(null));
+    }
+
+    @Test
+    public void testNode2StringWithOneNode(){
+        assertEquals("1,",Node.link2String(new Node(1)));
+    }
+
+}
