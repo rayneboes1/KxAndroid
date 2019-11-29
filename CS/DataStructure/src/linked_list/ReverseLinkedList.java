9dfd4d9c9fb3bedcd89f0c3ec27edca22efe54e9
wenhaiz <wenhaiz.me@qq.com>
2019-11-29 21:42:01 +0800

    增加反转链表的递归解法以及针对拥有两个结点的链表的测试案例

--- CS/DataStructure/src/linked_list/ReverseLinkedList.java
+++ CS/DataStructure/src/linked_list/ReverseLinkedList.java
@@ -8,6 +8,12 @@ package linked_list;
 public class ReverseLinkedList {
 
 
+    /**
+     * 循环解法
+     *
+     * @param list 待反转的链表
+     * @return 反转后的链表头结点
+     */
     public static Node resolve(Node list) {
         //链表为空或只有一个结点，直接返回
         if (list == null || list.next == null) {
@@ -26,6 +32,48 @@ public class ReverseLinkedList {
             list = next;
         }
         return newHead;
+    }
+
+
+    /**
+     * 递归解法
+     *
+     * @param list
+     * @return
+     */
+    public static Node resolveByRecursion(Node list) {
+        //如果是空链表，直接返回
+        if (list == null) {
+            return null;
+        }
+
+        //如果是只有一个结点的链表，也直接返回
+        //这个条件同时也是递归的终止条件
+        if (list.next==null){
+            return list;
+        }
+
+        //以上两句可以合并为
+//        if (list == null || list.next == null) {
+//            return list;
+//        }
+
+        //通过递归反转当前结点后的节点
+        Node reversedList = resolveByRecursion(list.next);
+
+        //此时 list.next 指向反转的链表的尾结点
+        //假设原链表为1->2->3->4->null，当前结点list为1
+        //则此时反转后的部分为 4->3->2->null
+        //而此时 list.next 指向 2
+
+        //将当前结点连接到反转后的链表的末尾，此时反转的链表为4->3->2->1
+        list.next.next = list;
+        //但当前结点1的next依然指向 2，因此需要把当前结点的 next 指向 null
+        list.next = null;
+        //通过此打印语句可以知道，返回的 reversedList 一直是原链表的尾结点
+        //通过画图搞清楚递归过程中的指向，特别是知道当前结点的next指向的是反转后的链表的尾结点，就比较好理解了
+//        System.out.println("反转后的链表头结点值为"+reversedList.data);
+        return reversedList;
     }
 
 }
--- CS/DataStructure/test/linked_list/ReverseLinkedListTest.java
+++ CS/DataStructure/test/linked_list/ReverseLinkedListTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 public class ReverseLinkedListTest {
 
-
     @Test
     public void testReverse(){
         Node list = new Node(1);
@@ -30,5 +29,48 @@ public class ReverseLinkedListTest {
         assertEquals("1,",Node.link2String(list));
         Node reverse = ReverseLinkedList.resolve(list);
         assertEquals("1,",Node.link2String(reverse));
+    }
+
+    @Test
+    public void testReverseWith2Node(){
+        Node list = new Node(1);
+        list.next = new Node(2);
+        assertEquals("1,2,",Node.link2String(list));
+        Node reverse = ReverseLinkedList.resolve(list);
+        assertEquals("2,1,",Node.link2String(reverse));
+    }
+
+    @Test
+    public void  testReverseByRecursion(){
+        Node list = new Node(1);
+        list.next = new Node(2);
+        list.next.next = new Node(3);
+        list.next.next.next = new Node(4);
+        assertEquals("1,2,3,4,",Node.link2String(list));
+        Node reverse = ReverseLinkedList.resolveByRecursion(list);
+        assertEquals("4,3,2,1,",Node.link2String(reverse));
+    }
+
+    @Test
+    public void testReverseByRecursionWithNull(){
+        Node reverse = ReverseLinkedList.resolveByRecursion(null);
+        assertEquals("",Node.link2String(reverse));
+    }
+
+    @Test
+    public void testReverseByRecursionWithOneNode(){
+        Node list = new Node(1);
+        assertEquals("1,",Node.link2String(list));
+        Node reverse = ReverseLinkedList.resolveByRecursion(list);
+        assertEquals("1,",Node.link2String(reverse));
+    }
+
+    @Test
+    public void testReverseByRecursionWith2Node(){
+        Node list = new Node(1);
+        list.next = new Node(2);
+        assertEquals("1,2,",Node.link2String(list));
+        Node reverse = ReverseLinkedList.resolveByRecursion(list);
+        assertEquals("2,1,",Node.link2String(reverse));
     }
 }
