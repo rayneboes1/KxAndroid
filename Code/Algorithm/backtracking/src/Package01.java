c688cf745e751952a57b9099f617ba37c79b60fa
wenhaiz <wenhaiz.me@qq.com>
2019-12-22 13:35:21 +0800

    optimize code

--- Code/Algorithm/backtracking/src/Package01.java
+++ Code/Algorithm/backtracking/src/Package01.java
@@ -3,13 +3,12 @@
  */
 public class Package01 {
 
-
     public static void main(String[] args) {
         Package01 package01 = new Package01();
 
-        int[] items = {1,3,5,7,8,9,11,20,12};
-        int result = package01.solution(items,74);
-        System.out.println("最大重量="+result);
+        int[] items = { 1, 3, 5, 7, 8, 9, 11, 20, 12 };
+        int result = package01.solution(items, 74);
+        System.out.println("最大重量=" + result);
 
     }
 
@@ -22,16 +21,16 @@ public class Package01 {
     }
 
     private void solutionInternal(int[] items, int i, int curWeight, int packageWeight) {
-        if (i == items.length) {
-            //已经到最后一个物品了
+        if (curWeight == packageWeight || i == items.length) {
+            // 已经到最后一个物品了
             if (curWeight > maxWeight) {
                 maxWeight = curWeight;
             }
             return;
         }
-        //不放入第i个物品
+        // 不放入第i个物品
         solutionInternal(items, i + 1, curWeight, packageWeight);
-        //放入第i个物品,对不符合条件的分支直接剪掉
+        // 放入第i个物品,对不符合条件的分支直接剪掉
         if (curWeight + items[i] <= packageWeight) {
             solutionInternal(items, i + 1, curWeight + items[i], packageWeight);
         }
